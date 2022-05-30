fb9176d93a86698a22c78d902ce23c1fbdf3e237
pankaj5417 <87115576+pankaj5417@users.noreply.github.com>
2022-05-30 13:49:07 +0530

    few changes doned

--- client/src/pages/signup/Signup.jsx
+++ client/src/pages/signup/Signup.jsx
@@ -1,66 +1,96 @@
-import { Button, TextField } from "@mui/material"
-import { useEffect } from "react"
-import { useState } from "react"
-import "./signup.css"
-export const SignUp=()=>{
+import { Button, TextField } from "@mui/material";
+import { useEffect } from "react";
+import { useState } from "react";
+import "./signup.css";
+export const SignUp = () => {
+  const [signup, setSignup] = useState({ username: "", password: "" });
 
-const [signup,setSignup]=useState({username:"",password:""})
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setSignup({ ...signup, [name]: value });
+  };
+  console.log(signup);
+  useEffect(() => {});
 
-    const handleChange=(e)=>{
-     const {name,value}=e.target
-     setSignup({...signup,[name]:value})
-    }
-console.log(signup)
-useEffect(()=>{
-
-})
-
-const signupUser=()=>{
-    fetch("http://localhost:8000/api/userData/register",{
-        method:"POST",
-        headers:{
-            "Content-Type":"application/json"
-        },
-        body:JSON.stringify(signup)
-        
-    }).then(res=>{
-        console.log("userpostdat",res)
-        return res.json()
-    }).then(user=>{
-        console.log(user)
-        setSignup(user)
-
+  const signupUser = () => {
+    fetch("http://localhost:8000/api/userData/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(signup),
     })
-    
-}
-    return (
-        <>
-        <div className="singup">
-            
-            <TextField  variant="standard" placeholder="Enter username" className="inputBox" type="text" name="name"  onChange={handleChange}/>
-            <br />
-            <TextField  variant="standard" placeholder="Enter email" className="inputBox" type="email" name="email"  onChange={handleChange}/>
-            <br />
-            <TextField  variant="standard"placeholder="Enter mobile number" className="inputBox" type="number" name="mobile"  onChange={handleChange}/>
-            <br />
-            <TextField  variant="standard" placeholder="Enter password" className="inputBox" type="password" name="password" onChange={handleChange}/>
-            <br />
-            <TextField  variant="standard" placeholder="Enter role" className="inputBox" type="text" name="role" onChange={handleChange}/>
-            <Button
-                variant="contained"
-                style={{
-                  backgroundColor: "#ff5722",
-                  height: "50px",
-                  fontWeight: "600",
-                }}
-                onClick={signupUser}
-                className="singupButton"
-              >
-                Sign Up
-              </Button>
-            
-        </div>
-        </>
-    )
-
-}
+      .then((res) => {
+        console.log("userpostdat", res);
+        return res.json();
+      })
+      .then((user) => {
+        console.log(user);
+        setSignup(user);
+      }).catch(err=>{
+        console.log(err)
+    })
+  };
+  return (
+    <>
+      <div className="singup">
+        <TextField
+          variant="standard"
+          placeholder="Enter username"
+          className="inputBox"
+          type="text"
+          name="name"
+          onChange={handleChange}
+        />
+        <br />
+        <TextField
+          variant="standard"
+          placeholder="Enter email"
+          className="inputBox"
+          type="email"
+          name="email"
+          onChange={handleChange}
+        />
+        <br />
+        <TextField
+          variant="standard"
+          placeholder="Enter mobile number"
+          className="inputBox"
+          type="number"
+          name="mobile"
+          onChange={handleChange}
+        />
+        <br />
+        <TextField
+          variant="standard"
+          placeholder="Enter password"
+          className="inputBox"
+          type="password"
+          name="password"
+          onChange={handleChange}
+        />
+        <br />
+        <TextField
+          variant="standard"
+          placeholder="Enter role"
+          className="inputBox"
+          type="text"
+          name="role"
+          onChange={handleChange}
+        />
+        <Button
+          variant="contained"
+          style={{
+            backgroundColor: "#ff5722",
+            height: "50px",
+            fontWeight: "600",
+          }}
+          onClick={signupUser}
+          className="singupButton"
+        >
+          Sign Up
+        </Button>
+      </div>
+    </>
+  );
+};
